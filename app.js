0d3317cf4cf9dd60a727972b0c545a6ea3098132
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-09-27 21:00:48 -0300

    connection successfull

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = 3000;
 
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || port);
+http.listen(process.env.PORT || port);
 console.log('Express started on port ' + port);
